012524270803ef35fc3897ee788c714d9977707c
Alejandro González Santiago <agonsant@everis.com>
2020-04-29 05:36:41 +0200

    initial EXERCISE 5

--- src/game.ts
+++ src/game.ts
@@ -1,21 +1,74 @@
 import { Board } from "./board.js";
 import { Snake } from "./snake.js";
+import { Point } from "./models/models.js";
 
 export class Game {
 
     private board: Board;
     private snake: Snake;
-    private static grid: number = 16;
+    private static grid: number = 10;
+    private frameCount: number;
+    private gameOver: boolean;
 
     constructor() {
         const canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('game');
         this.board = new Board(canvas);
         this.snake = new Snake({ dx: Game.grid, dy: 0 }, { x: 160, y: 160 }, 4);
+        this.frameCount = 0;
+        this.gameOver = false;
+        this.registerActions();
     }
 
+    /**
+     * Add The listeners for moving the snake
+     */
+    private registerActions(): void {
+        document.addEventListener('keydown', (e: KeyboardEvent) => {
+            // prevent snake from backtracking on itself by checking that it's 
+            // not already moving on the same axis (pressing left while moving
+            // left won't do anything, and pressing right while moving left
+            // shouldn't let you collide with your own body)
+            const snakeDirection = this.snake.getDirection();
+            if (e.which === 37 && snakeDirection.dx === 0) { // left arrow key
+                this.snake.changeDirection({ dx: -Game.grid, dy: 0 });
+            } else if (e.which === 38 && snakeDirection.dy === 0) { // up arrow key
+                this.snake.changeDirection({ dx: 0, dy: -Game.grid });
+            } else if (e.which === 39 && snakeDirection.dx === 0) { // right arrow key
+                this.snake.changeDirection({ dx: Game.grid, dy: 0 });
+            } else if (e.which === 40 && snakeDirection.dy === 0) { // down arrow key
+                this.snake.changeDirection({ dx: 0, dy: Game.grid });
+            }
+        });
+    }
+
+    /**
+     * There is a Snake collision if one the head is in the same position as 
+     * other different cell
+     */
+    private isSnakeCollision(): boolean {
+        const snakeCells: Point[] = this.snake.getCells();
+        return snakeCells.some((c: Point, i: number) => {
+            return i > 0 && c.x === snakeCells[0].x && c.y === snakeCells[0].y;
+        });
+    }
+
+    /**
+     * Main Game Loop:
+     *   Requests a new Frame
+     *   Moves the Snake
+     *   Draws 
+     *   Check Game Over
+     */
     start() {
-        requestAnimationFrame(this.start.bind(this));
-        this.snake.move();
-        this.board.draw(this.snake, Game.grid);
+        if (!this.gameOver) {
+            requestAnimationFrame(this.start.bind(this));
+            if (++this.frameCount < 6) return;
+            this.frameCount = 0;
+            this.snake.move(this.board.getHeight(), this.board.getWidth(), Game.grid);
+            this.board.draw(this.snake, Game.grid);
+            this.gameOver = this.isSnakeCollision();
+        } else {
+            this.board.gameOver();
+        }
     }
 }
--- src/snake.ts
+++ src/snake.ts
@@ -20,14 +20,54 @@ export class Snake {
         }
     }
 
-    move(): void {
+    private calculateLimitPosition(maxHeight: number, maxWidth: number, grid: number): Partial<Point> {
+        const limitPosition: Partial<Point> = {};
+        // wrap snake position horizontally on edge of the board
+        if (this.head.x < 0) {
+            limitPosition.x = maxWidth - grid;
+        } else if (this.head.x >= maxWidth) {
+            limitPosition.x = 0;
+        }
+
+        // wrap snake position vertically on edge of the board
+        if (this.head.y < 0) {
+            limitPosition.y = maxHeight - grid;
+        } else if (this.head.y >= maxHeight) {
+            limitPosition.y = 0;
+        }
+        return limitPosition;
+    }
+
+    /**
+     * Moves the Snake One position removing the last cell and creating a new head
+     */
+    move(maxHeight: number, maxWidth: number, grid: number): void {
         this.body.unshift({ x: this.head.x, y: this.head.y });
         this.body.pop();
         this.head.x += this.velocity.dx;
         this.head.y += this.velocity.dy;
+        this.head = { ...this.head, ...this.calculateLimitPosition(maxHeight, maxWidth, grid) };
     }
 
+    /**
+     * Retrieves the whole snake cells
+     */
     getCells(): Point[] {
         return [this.head, ...this.body];
+    }
+
+    /**
+     * Retrieves the Snake direction Vector
+     */
+    getDirection(): Vector {
+        return this.velocity;
+    }
+
+    /**
+     * Changes the Snake direction
+     * @param newDirection the new Snake direction
+     */
+    changeDirection(newDirection: Vector): void {
+        this.velocity = newDirection;
     }
 }
